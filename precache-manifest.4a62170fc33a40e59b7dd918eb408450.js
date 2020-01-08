deca2e66bbfdb93803d9479d31ab1cbf55502fd7
Jose R. Guaura <jguaura@gmail.com>
2020-01-08 00:33:19 -0300

    Updates

---
+++ precache-manifest.4a62170fc33a40e59b7dd918eb408450.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c5a6767fc753698b4bd14125356ccd",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "61b05fc8b912001a4f8f",
+    "url": "/monsters-app/static/css/main.e49004d6.chunk.css"
+  },
+  {
+    "revision": "4d525530b917453e410e",
+    "url": "/monsters-app/static/js/2.263f51ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-app/static/js/2.263f51ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61b05fc8b912001a4f8f",
+    "url": "/monsters-app/static/js/main.a7a9edfb.chunk.js"
+  },
+  {
+    "revision": "125b65c6eeeba7550098",
+    "url": "/monsters-app/static/js/runtime-main.2dc06fbe.js"
+  }
+]);
